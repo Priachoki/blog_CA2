5bbb3b84147a0007fe341f218310f90a1140ddcc
Priyanka <priyankakwambokaachoki@gmail.com>
2025-03-14 02:57:29 +0000

    Changed The Background Image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,23 +2,27 @@
 
 @section('content')
     <div class="background-image grid grid-cols-1 m-auto">
-        <div class="flex text-gray-100 pt-10">
-            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
-                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
-                </h1>
-                <a 
-                    href="/blog"
-                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
-                    Read More
-                </a>
+        <header>
+           
+            <div class="content">
+                <div class="flex text-gray-100 pt-10">
+                    <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+                        <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                            If all the world's a stage, put me in the front row
+                        </h1>
+                        <a 
+                            href="/blog"
+                            class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                            Read More
+                        </a>
+                    </div>
+                </div>
             </div>
-        </div>
+        </header>
     </div>
 
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
         </div>
 
         <div class="m-auto sm:m-auto text-left w-4/5 block">
@@ -95,6 +99,7 @@
         </div>
         <div>
             <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="">
-        </div>
+        </div>            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
+
     </div>
 @endsection
