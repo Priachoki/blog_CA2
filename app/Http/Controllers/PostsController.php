3d067e0879324950aa12ce5f6237f0c889bb1877
Priyanka <priyankakwambokaachoki@gmail.com>
2025-03-18 21:33:42 +0000

    Fixed the issue in the PostController page

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,45 +1,29 @@
 <?php
 
-namespace App\Http\ers;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
 use Cviebrock\EloquentSluggable\Services\SlugService;
 
-class Postser extends er
+class PostsController extends Controller
 {
- 
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         return view('blog.index')
             ->with('posts', Post::orderBy('updated_at', 'DESC')->get());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('blog.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -64,37 +48,18 @@ class Postser extends er
             ->with('message', 'Your post has been added!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  string  $slug
-     * @return \Illuminate\Http\Response
-     */
     public function show($slug)
     {
         return view('blog.show')
             ->with('post', Post::where('slug', $slug)->first());
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  string  $slug
-     * @return \Illuminate\Http\Response
-     */
     public function edit($slug)
     {
         return view('blog.edit')
             ->with('post', Post::where('slug', $slug)->first());
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  string  $slug
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $slug)
     {
         $request->validate([
@@ -114,12 +79,6 @@ class Postser extends er
             ->with('message', 'Your post has been updated!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($slug)
     {
         $post = Post::where('slug', $slug);
@@ -129,4 +88,3 @@ class Postser extends er
             ->with('message', 'Your post has been deleted!');
     }
 }
-
